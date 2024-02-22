ce86255449785910b81d123b8f9d61bb1071d965
JibrilBarkindo <jbarkindo@gmail.com>
2024-02-22 20:22:33 +0000

    added challenges page
    added challenges page

---
+++ app/src/main/java/com/example/mindkeep/ChallengesPage.java
@@ -0,0 +1,14 @@
+package com.example.mindkeep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChallengesPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenges_page);
+    }
+}
